69aefa1ec2c6e2da44e65df1e08b9a82c61e9700
Bartłomiej Smykla <bartek@smykla.com>
2017-11-06 19:05:41 +0000

    added asyncValidate functionality

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import DefaultTheme from './themes/bootstrap3'
 import { reduxForm } from 'redux-form'
+import noop from 'lodash/noop'
+
+import DefaultTheme from './themes/bootstrap3'
 import renderFields from './renderFields'
 import renderField from './renderField'
 import processSubmitErrors from './processSubmitErrors'
@@ -11,6 +13,7 @@ import compileSchema from './compileSchema'
 
 const BaseForm = props => {
     const { schema, handleSubmit, theme, error, submitting, context } = props
+
     return (
         <form onSubmit={handleSubmit}>
             {renderField(schema, null, theme || DefaultTheme, '', context)}
@@ -18,21 +21,50 @@ const BaseForm = props => {
                 {error && <strong>{error}</strong>}
             </div>
             <button className="btn btn-primary" type="submit" disabled={submitting}>Submit</button>
-        </form>)
+        </form>
+    )
 }
 
 const Liform = (props) => {
-    props.schema.showLabel = false
-    const schema = compileSchema(props.schema)
-    const formName = props.formKey || props.schema.title || 'form'
+    const {
+        schema,
+        formKey,
+        syncValidation,
+        ajv,
+        initialValues,
+        context,
+        baseForm,
+        asyncValidate,
+    } = props
+    const { title } = schema
+
+    const newProps = {
+      ...props,
+      schema: {
+        ...props.schema,
+        showLabel: true,
+      }
+    }
+
+    const newContext = {
+      ...context,
+      formName,
+    }
+
+    const compiledSchema = compileSchema(schema)
+    const formName = formKey || title || 'form'
+    const validate = syncValidation || buildSyncValidation(compiledSchema, ajv)
+
     const FinalForm = reduxForm({
-        form: props.formKey || props.schema.title || 'form',
-        validate: props.syncValidation || buildSyncValidation(schema, props.ajv),
-        initialValues: props.initialValues,
-        context: { ...props.context, formName },
-    })(props.baseForm || BaseForm)
+        form: formName,
+        validate,
+        initialValues,
+        asyncValidate,
+        context: newContext,
+    })(baseForm || BaseForm)
+
     return (
-        <FinalForm renderFields={renderField.bind(this)} {...props} schema={schema} />
+        <FinalForm renderFields={renderField.bind(this)} {...newProps} schema={compiledSchema} />
     )
 }
 
@@ -41,10 +73,15 @@ Liform.propTypes = {
     onSubmit: PropTypes.func,
     initialValues: PropTypes.object,
     syncValidation: PropTypes.func,
+    asyncValidate: PropTypes.func,
     formKey: PropTypes.string,
     baseForm: PropTypes.func,
     context: PropTypes.object,
     ajv: PropTypes.object,
+}
+
+Liform.defaultProps = {
+    asyncValidate: noop,
 }
 
 export default Liform
